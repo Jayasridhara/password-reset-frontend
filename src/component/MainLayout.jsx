05b075fcb288eb3fbc4a0b8fd22c17129a1cbeec
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-09-29 07:15:42 +0530

    push

--- src/component/MainLayout.jsx
+++ src/component/MainLayout.jsx
@@ -7,23 +7,6 @@ const MainLayout = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Password Reset App</Link>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/forgot-password">Forgot Password</Link>
-                            </li>
-                            {/* Add other navigation links here as needed */}
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">Register</Link>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
             </nav>
             <div className="container mt-5">
